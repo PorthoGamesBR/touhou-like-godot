fd6226a788045c0fe6b36902426d6255c865a977
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2024-10-25 15:27:30 -0300

    Adicionado novo script e novo padrão de projetil

--- scripts/projeteis/projetil.gd
+++ scripts/projeteis/projetil.gd
@@ -1,11 +1,17 @@
 extends Sprite2D
 
+@onready var timer : Timer = $Timer
+
+@export var despawn_time_sec: float = 5.0
+
 var direction = Vector2.UP
 var velocity = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if despawn_time_sec > 0:
+		timer.wait_time = despawn_time_sec
+		timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
